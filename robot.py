35fa4477a6a1a3b13b8dcb9d7b39a026a9d430d1
WillH001 <whare23@choate.edu>
2022-05-16 11:55:03 -0400

    Code to drive forwards 7 feet at 40% speed

--- robot.py
+++ robot.py
@@ -5,15 +5,18 @@ from drivetrain import Drivetrain
 
 
 class Robot(wpilib.TimedRobot):
-    
-    joy1=Joystick(0)
-    
+    joy1 = Joystick(0)
+
     def __init__(self):
         super().__init__()
         self.drivetrain = Drivetrain()
 
     def robotInit(self):
-        pass
+        self.drivetrain.m_left_encoder.setPosition(0)
+        self.drivetrain.m_right_encoder.setPosition(0)
+
+        self.controller = PIDController(P, I, D)
+        self.controller.setTolerance(15)
 
     def robotPeriodic(self):
         pass
@@ -22,16 +25,22 @@ class Robot(wpilib.TimedRobot):
         pass
 
     def teleopPeriodic(self):
-        speed=-self.joy1.getRawAxis(1)*.4
-        turn=self.joy1.getRawAxis(4)*.2
-        #print(f"Speed: {speed} Turn: {turn}")
-        self.drivetrain.set(speed+turn, speed-turn)
+        speed = -self.joy1.getRawAxis(1) * .4
+        turn = self.joy1.getRawAxis(4) * .2
+        # print(f"Speed: {speed} Turn: {turn}")
+        self.drivetrain.set(speed + turn, speed - turn)
 
     def autonomousInit(self):
-        pass
+        self.wheel_diameter = 4  # inches
+        self.wheel_circumference = self.wheel_diameter * math.pi
+        self.leg_1_distance = (84)
+        self.leg_1_clicks = (self.leg_1_distance / self.wheel_circumference) * 360
+        self.drivetrain.set(self.leg_1_clicks, self.leg_1_clicks)
 
     def autonomousPeriodic(self):
-        pass
+        self.velocity = self.controller.calculate(measurement=self.drivetrain.m_left_encoder.getPosition())
+        self.velocity = min(0.4, max(-.4, self.velocity))
+        self.drivetrain.set(self.velocity, self.velocity)
 
 
 if __name__ == "__main__":
